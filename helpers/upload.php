aaca4f6a19ad6ee7b8f1582111a2845168b769b1
alonso <alons182@hotmail.com>
2013-11-11 19:42:38 -0600

    guanaprint upload

--- helpers/upload.php
+++ helpers/upload.php
@@ -5,16 +5,21 @@
 	$uploadfile = $uploaddir . basename($_FILES['uploadBtn']['name']);
 	
        
-       
-	
-	if (move_uploaded_file($_FILES['uploadBtn']['tmp_name'], $uploadfile)) {
-	   $data = array('message' => 'File is valid, and was successfully uploaded.');
+   if ($_FILES['uploadBtn']['size'] <= 2000000) { 
+
+    	if (move_uploaded_file($_FILES['uploadBtn']['tmp_name'], $uploadfile)) {
+	   		$data = array('message' => 'File is valid, and was successfully uploaded.');
 	   
-	} else {
-	   $data = array('message' => 'Error');
-	}
+		} else {
+		   $data = array('message' => 'Error');
+		}
+    }else
+   		 $data = array('message' => 'Muy grande');
+	
+	
 
 	//$file = 
 	//echo $data['email'] .' - '. $_FILES['uploadBtn']['name'];;
+   		
 	echo json_encode($data);
 ?>
