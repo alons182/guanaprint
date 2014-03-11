550977c1cba9b3c11c820527bf2e0fc91c13059f
alonso <alons182@hotmail.com>
2014-03-10 19:16:25 -0600

    envio de email upload

--- helpers/upload.php
+++ helpers/upload.php
@@ -8,7 +8,39 @@
    if ($_FILES['uploadBtn']['size'] <= 209715200) { 
    	
     	if (move_uploaded_file($_FILES['uploadBtn']['tmp_name'], $uploadfile)) {
-	   		$data = array('message' => 'Archivo subido Exitosamente.');
+	   		
+
+	   		$result = 'ok';
+			if(trim($_POST['nombre']) === '') {
+				$result = 'Error - falta el campo nombre';
+			}else{
+				$name = trim($_POST['nombre']);
+			}
+			if(trim($_POST['email']) === '') {
+				$result = 'Error - falta el campo email';
+			}else{
+				$email = trim($_POST['email']);
+			}
+			if(trim($_POST['descripcion']) === '') {
+				$result = 'Error - falta el campo comentarios';
+			}else{
+				$comments = trim($_POST['descripcion']);
+			}
+
+			if($result=='ok'){
+				$emailTo = 'alonso@avotz.com';
+			    $subject = 'Desde el formulario de Archivo del Sitio Guanaprint - Submitted message from '.$name;
+			    $body = "Nombre: $name \n\nEmail: $email \n\nComments: $comments";
+			    $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+			    mail($emailTo, $subject, $body, $headers);
+			        
+			    
+			    
+			}
+			
+			$data = array('message' => 'Archivo subido Exitosamente.');
+
 	   
 		} else {
 		   $data = array('message' => 'Error al subir el archivo. Verifique que no sobrepase el limite de 64mb');
