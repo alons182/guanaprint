6c6cc64dea91513afcf88be2f4c6aa6fc4e6c2ef
alonso <alons182@hotmail.com>
2014-03-10 20:10:51 -0600

    ***

--- helpers/upload.php
+++ helpers/upload.php
@@ -27,7 +27,9 @@
 				$comments = trim($_POST['descripcion']);
 			}
 
-			$link = '<a href="'.$_SERVER['REQUEST_URI'].'/uploads/'.$_FILES['uploadBtn']['name'].'" title="Imagen">link</a>';
+ 			$link = '<a href="http://"'. $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"].'/uploads/'.$_FILES['uploadBtn']['name'].'" title="Imagen">link</a>';
+
+	
 
 			if($result=='ok'){
 				$emailTo = 'alonso@avotz.com';
