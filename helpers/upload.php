a7d4ed59bccf2bab7677762833ba4d2f664a4cc9
alonso <alons182@hotmail.com>
2014-03-10 21:04:49 -0600

    ***

--- helpers/upload.php
+++ helpers/upload.php
@@ -44,7 +44,7 @@
 				$body .= "<tr><td><strong>URL del Archivo:</strong> </td><td>" . $link . "</td></tr>";
 				$body .= "</table>";
 				$body .= "</body></html>";
-			    $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email . "\r\nCC:".$email."\r\n". "MIME-Version: 1.0\r\n"."Content-Type: text/html; charset=ISO-8859-1\r\n";
+			    $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email . "\r\nCC:".$email."\r\n". "MIME-Version: 1.0\r\n"."Content-Type: text/html; charset=utf-8\r\n";
 
 
 			    mail($emailTo, $subject, $body, $headers);
